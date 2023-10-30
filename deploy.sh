c82ebd22458cd55456a52b954982f00e8273020e
shanmugamrak <139672847+shanmugamrak@users.noreply.github.com>
2023-10-30 18:07:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,17 @@
 process_line() {
   local filename
-  local source  # Add a new variable for the source location
+  local source
   local destination
 
-  # Split the line into filename, source, and destination using a comma as the delimiter
   IFS=',' read -r filename source destination <<< "$1"
 
-  # Perform actions based on the values
   echo "Deploying $filename from $source to $destination"
-  
-  # Check if either the filename, source, or destination is empty
+
   if [ -z "$filename" ] || [ -z "$source" ] || [ -z "$destination" ]; then
     echo "Error: Empty filename, source, or destination in line: $1"
     return
   fi
 
-  # Backup the file before deploying
   if [ -e "$destination" ]; then
     backup_dir="backup"
     mkdir -p "$backup_dir"
@@ -25,9 +21,7 @@ process_line() {
   else
     echo "File $destination not found, skipping backup"
   fi
-  
-  # Add your deployment logic here
-  # For example, you can use 'cp' to copy the file to the destination:
+
   cp "$source/$filename" "$destination"
 
   if [ $? -eq 0 ]; then
@@ -37,11 +31,8 @@ process_line() {
   fi
 }
 
-# Check if the release.txt file exists
 if [ -e "release.txt" ]; then
-  # Read each line from the release.txt file
   while IFS= read -r line; do
-    # Skip empty lines and lines starting with #
     if [[ -n "$line" && "$line" != "#"* ]]; then
       process_line "$line"
     fi
