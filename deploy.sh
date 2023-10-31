4c8b05102df1c30641c659fa69377f8fea4ebd9b
shanmugamrak <139672847+shanmugamrak@users.noreply.github.com>
2023-10-31 17:25:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,43 @@
 process_line() {
   local filename
-  local source
   local destination
 
-  IFS=',' read -r filename source destination <<< "$1"
+  IFS=',' read -r filename destination <<< "$1"
 
-  echo "Deploying $filename from $source to $destination"
+  echo "Deploying $filename to $destination"
 
-  if [ -z "$filename" ] || [ -z "$source" ] || [ -z "$destination" ]; then
-    echo "Error: Empty filename, source, or destination in line: $1"
+  if [ -z "$filename" ] || [ -z "$destination" ]; then
+    echo "Error: Empty filename or destination in line: $1"
     return
   fi
 
+  # Backup the file before deploying
   if [ -e "$destination" ]; then
     backup_dir="/home/ubuntu/backup"
     mkdir -p "$backup_dir"
     backup_file="$backup_dir/$(basename "$destination")_backup_$(date +'%Y%m%d%H%M%S')"
-    cp -r "$destination" "$backup_file"
+    cp "$destination" "$backup_file"
     echo "Backed up $destination to $backup_file"
-
-    # Create a symbolic link to the latest backup
-    latest_backup_link="/home/ubuntu/latest_backup"
-    rm -f "$latest_backup_link"  # Remove the previous symlink if it exists
-    ln -s "$backup_file" "$latest_backup_link"
   else
     echo "File $destination not found, skipping backup"
   fi
-
-  cp "$source/$filename" "$destination"
+  
+  # Add your deployment logic here
+  # For example, you can use 'cp' to copy the file from the source to the destination:
+  cp "$filename" "$destination"
 
   if [ $? -eq 0 ]; then
-    echo "Copied $filename from $source to $destination"
+    echo "Copied $filename to $destination"
   else
-    echo "Error copying $filename from $source to $destination"
+    echo "Error copying $filename to $destination"
   fi
 }
 
+# Check if the release.txt file exists
 if [ -e "release.txt" ]; then
+  # Read each line from the release.txt file
   while IFS= read -r line; do
+    # Skip empty lines and lines starting with #
     if [[ -n "$line" && "$line" != "#"* ]]; then
       process_line "$line"
     fi
